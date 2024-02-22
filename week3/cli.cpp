9578940919048f745b5562ada8e5926afa2a1ead
ks2019575010 <48661594+ks2019575010@users.noreply.github.com>
2024-02-22 21:33:48 +0900

    Update cli.cpp

--- week3/cli.cpp
+++ week3/cli.cpp
@@ -6,14 +6,15 @@ int main() {
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    //char ip[16] = "";
-    //cout << "Input server IP: "; cin >> ip;
+    char ip[16] = "";
+    cout << "Input server IP: "; 
+    cin >> ip;
 
     // 서버 주소 설정
     sockaddr_in serverAddr;
     ZeroMemory(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr("192.168.219.104"); // 서버의 IP 주소
+    serverAddr.sin_addr.s_addr = inet_addr(ip); // 서버의 IP 주소
     serverAddr.sin_port = htons(12345); // 서버의 포트 번호
 
     // 소켓 생성
@@ -36,19 +37,6 @@ int main() {
     int recvBufLen = DEFAULT_BUFLEN;
 
     while (true) {
-        // 서버로부터 메시지 받기
-        int bytesReceived = recv(clientSocket, recvBuf, recvBufLen, 0);
-        if (bytesReceived > 0) {
-            recvBuf[bytesReceived] = '\0';
-            std::cout << "Server: " << recvBuf << std::endl;
-        } else if (bytesReceived == 0) {
-            std::cerr << "Connection closed by server" << std::endl;
-            break;
-        } else {
-            std::cerr << "recv failed with error: " << WSAGetLastError() << std::endl;
-            break;
-        }
-
         // 사용자로부터 메시지 입력 받기
         std::cout << "Enter message: ";
         std::string message;
@@ -60,6 +48,18 @@ int main() {
             std::cerr << "send failed with error: " << WSAGetLastError() << std::endl;
             break;
         }
+        // 서버로부터 메시지 받기
+        int bytesReceived = recv(clientSocket, recvBuf, recvBufLen, 0);
+        if (bytesReceived > 0) {
+            recvBuf[bytesReceived] = '\0';
+            std::cout << "other: " << recvBuf << std::endl;
+        } else if (bytesReceived == 0) {
+            std::cerr << "Connection closed by server" << std::endl;
+            break;
+        } else {
+            std::cerr << "recv failed with error: " << WSAGetLastError() << std::endl;
+            break;
+        }
     }
 
     // 소켓 닫기 및 Winsock 종료
@@ -68,3 +68,5 @@ int main() {
 
     return 0;
 }
+//192.168.219.104
+//192.168.1.36
